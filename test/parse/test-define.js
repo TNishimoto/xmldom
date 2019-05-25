15bdc8b1d3685f0e28fe0d565fc70251a415b2e1
nishimoto <a32b16c4@gmail.com>
2019-05-25 20:26:07 +0900

    added tsconfig.json

--- test/parse/test-define.js
+++ test/parse/test-define.js
@@ -1,3 +1,3 @@
 console.log(__dirname)
-exports.XMLSerializer = require('../../dom').XMLSerializer ;
-exports.DOMParser = require('../../dom-parser').DOMParser;
+exports.XMLSerializer = require('../../dist/dom').XMLSerializer ;
+exports.DOMParser = require('../../dist/dom-parser').DOMParser;
